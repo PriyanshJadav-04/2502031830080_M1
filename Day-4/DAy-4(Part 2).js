3c8a0369f1dea4be63bc97d5acd35ff5957acaad
Priyansh Jadav <thepriyanshjadhav@gmail.com>
2026-05-14 14:13:07 +0530

    Day 4 (Part 2)

---
+++ Day-4/DAy-4(Part 2).js
@@ -0,0 +1,62 @@
+// let a = 1;
+// console.log(a++);
+// console.log(++a);
+// console.log(a--);
+// console.log(--a);
+
+// let b = 5;
+// let c = 10;
+// let e = b == c;
+// let f = b === c;
+// let g = b != c;
+// let h = b !== c;
+// let i = b >= c;
+// let j = b <= c;
+// console.log(d, e, f, g, h, i, j);
+
+//------------------ 1. Check Voting Eligbilty------------------
+// let a = prompt();
+// if(a >= 18){
+//     console.log("You're Eligible for voting")
+// } else {
+//     console.log("You're not Eligible for voting")
+// }
+// document.getElementById("voting").innerHTML = "Your Age is: " + a;
+
+//------------------ 2. Find Poitive or Negative Number------------------
+// let a = prompt("Enter a number:");
+// if (a > 0) {
+//   console.log("Positive");
+// } else if (a < 0) {
+//   console.log("Negative");
+// } else {
+//   console.log("Zero");
+// }
+
+//------------------ 3. Find Greatest of Two Number------------------
+// let a = Number(prompt("Enter the first number"));
+// let b = Number(prompt("Enter the second number"));
+// if (a > b) {
+//   console.log(a + " is greater than " + b);
+// } else if (b > a) {
+//   console.log(b + " is greater than " + a);
+// } else if (a === b) {
+//   console.log("Both numbers are equal");
+// } else {
+//   console.log("Invalid Input");
+// }
+
+// 4.------------------ Check Username and Password------------------
+// let user = "Priyansh"; 
+// let pass = "123"; 
+// let check = prompt("Enter Username"); 
+// let checkpass = prompt("Enter Password"); 
+// if (check === user && checkpass === pass) { 
+//     console.log("Successfully Logged In"); 
+// } else if (check !== user && checkpass === pass) { 
+//     console.log("Username is wrong"); 
+// } else if (check === user && checkpass !== pass) { 
+//     console.log("Password is wrong"); 
+// } else { 
+//     console.log("Username and Password are wrong"); 
+// }
