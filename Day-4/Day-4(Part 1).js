0127e02785454c04ad333e975044122a0bd0c3f6
Priyansh Jadav <thepriyanshjadhav@gmail.com>
2026-05-14 11:44:11 +0530

    Day 4 (Part 1)

---
+++ Day-4/Day-4(Part 1).js
@@ -0,0 +1,17 @@
+// console.log("Hello World");
+
+// alert("Hello World");
+// confirm("Hello World");
+// prompt();
+
+document.getElementsByClassName("heading")[0].innerHTML = "Javascript 01";
+document.getElementsByClassName("heading")[1].innerHTML = "Javascript 02";
+document.getElementById("firstText").innerHTML = "Hello World";
+
+let x = 15;
+let y = 20;
+let z = x + y * x / y - x;
+document.getElementById("Sum").innerHTML = "Vaule of the Z : " + z;
+
+let a = true;
+console.log(typeof a)
