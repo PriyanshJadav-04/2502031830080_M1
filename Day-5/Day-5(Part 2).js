6ef3f340bef1f09d56bf390a0ef5f68743d53043
Priyansh Jadav <thepriyanshjadhav@gmail.com>
2026-05-15 14:25:29 +0530

    Day 5 (Part 2)

---
+++ Day-5/Day-5(Part 2).js
@@ -0,0 +1,60 @@
+//------------------ 1. Array------------------
+// let car = new Array(3);
+// car[0] = "Ford";
+// car[1] = "Toyota";
+// car[2] = "Honda";
+// let car2 = new Array("Ford", "Toyota", "Honda");
+// let car3 = ["Audi", "Innova", "WagonR"];
+// document.write(car2 + "<br>");
+// car2.push("Yamaha");
+// document.write(car2 + "<br>");
+// car2.pop();
+// document.write(car2 + "<br>");
+// let data = car2.concat(car3);
+// document.write(data);
+
+//------------------ 2. Function------------------
+// function foo(p1) {
+//   if (typeof p1 == "number") return 0;
+//   else if (typeof p1 == "string") return "zero";
+// }
+// console.log(foo(1));
+// console.log(foo("abc"));
+// console.log(foo());
+
+//------------------ 3. Function Sum------------------
+// function sum() {
+//   let s = 0;
+//   for (let i = 0; i < arguments.length; i++) {
+//     s += arguments[i];
+//   }
+//   return s;
+// }
+// var a = sum(1, 2, 3);
+// var a = sum(1, 2, 3, 4, 5);
+// var a = sum(1, 2, "3", 4, 5);
+// document.write(a);
+
+//------------------ 4. String Method------------------
+// let text = " Hello JavaScript ";
+// let text2 = "JavaScript";
+// let msg = "cat dog cat";
+// console.log(text.length);
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+// console.log(text.charAt(1));
+// console.log(text.indexOf("Java"));
+// console.log(text.lastIndexOf("a"));
+// console.log(text.includes("Java"));
+// console.log(text.trim().startsWith("Hello"));
+// console.log(text.trim().endsWith("JavaScript"));
+// console.log(text.slice(1, 6));
+// console.log(text.substring(1, 6));
+// console.log(text.substr(1, 5));
+// console.log(text.replace("Hello", "Hi"));
+// console.log(msg.replaceAll("cat", "lion"));
+// console.log(text.trim());
+// console.log(text.valueOf());
+// console.log(text.search("Java"));
+// console.log("apple".localeCompare("banana"));
+q
